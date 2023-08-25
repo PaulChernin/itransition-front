715e590d4d8d58f839f0af99f7128bb6a5bfd07e
PaulChernin <pavel@chernin.ru>
2023-08-25 23:01:06 +0300

    add basic style to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,19 @@
+import { Flex, useColorModeValue } from "@chakra-ui/react"
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher"
 import ThemeModeSwitcher from "../ThemeModeSwitcher/ThemeModeSwitcher"
 
 const Header = () => {
     return <>
-        <LanguageSwitcher/>
-        <ThemeModeSwitcher/>
+        <Flex
+            as='header'
+            p={2}
+            borderBottom={1}
+            borderStyle='solid'
+            borderColor={useColorModeValue('gray.800', 'gray.200')}
+        >
+            <LanguageSwitcher/>
+            <ThemeModeSwitcher/>
+        </Flex>
     </>
 }
 
