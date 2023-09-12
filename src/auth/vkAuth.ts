d74331e259cf687cd173e6a3c6c94fccc36320d8
PaulChernin <pavel@chernin.ru>
2023-09-13 01:01:39 +0300

    refactor vkAuth

--- src/auth/vkAuth.ts
+++ src/auth/vkAuth.ts
@@ -1,6 +1,5 @@
 import { Config, Connect} from '@vkontakte/superappkit'
 import axios from '../api/axios'
-import { Profile } from '../hooks/profile/Profile'
 
 Config.init({
   appId: 51736723
@@ -14,16 +13,35 @@ const saveToken = (token: string) => {
   localStorage.setItem('token', token)
 }
 
-export const handleSilentToken = async (token: string) => {
+type dbProfile = {
+    id: number,
+    nick: string,
+    isAdmin: boolean
+}
+
+type ExchangeTokensResponse = {
+  user: dbProfile,
+  token: string
+}
+
+const exchangeTokens = async (silent: string) => {
   const response = await axios.post('/vk-auth', {
-    silentToken: token
+    silentToken: silent
   })
-  const jwt: string = response.data.token
-  saveToken(jwt)
-  const profile: Profile = {
-    userId: response.data.user.id,
-    userNick: response.data.user.nick,
-    isAdmin: response.data.user.isAdmin
+  return response.data as ExchangeTokensResponse
+}
+
+const mapProfile = (profile: dbProfile) => {
+  return {
+    userId: profile.id,
+    userNick: profile.nick,
+    isAdmin: profile.isAdmin
   }
-  return profile
+}
+
+export const handleSilentToken = async (silent: string) => {
+  const { user, token } = await exchangeTokens(silent)
+  const jwt: string = token
+  saveToken(jwt)
+  return mapProfile(user)
 }
