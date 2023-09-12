fda0bc437fb3cfb21d272ebe4339f63d09cfff42
PaulChernin <pavel@chernin.ru>
2023-09-12 23:56:48 +0300

    refactor main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { ChakraProvider } from "@chakra-ui/react"
 import theme from "./theme/theme"
 import { RouterProvider } from "react-router-dom"
 import router from "./router/router"
+import './i18n/config.ts'
+import "easymde/dist/easymde.min.css"
 
 const App = () => {
-
     return (
         <ChakraProvider theme={theme}>
             <RouterProvider router={router} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import './i18n/config.ts'
-import "easymde/dist/easymde.min.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')!
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
