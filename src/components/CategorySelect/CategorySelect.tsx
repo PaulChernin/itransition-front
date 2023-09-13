4ac14d02144dd06c2ff243c622367014e80df525
PaulChernin <pavel@chernin.ru>
2023-09-14 00:09:33 +0300

    fix select import bug

--- src/components/CategorySelect/CategorySelect.tsx
+++ src/components/CategorySelect/CategorySelect.tsx
@@ -1,17 +1,17 @@
 import { useTranslation } from "react-i18next"
-import Select from "../../ui/Select"
+import Select from "../../ui/Select/Select"
 
 type CategorySelectProps = {
     category: string,
     setCategory: (value: string) => void
 }
 
-const categories = ['book', 'game', 'movie']
+const categories = ['book', 'game', 'movie'] // TODO: вынести в отдельный файл
 
 const CategorySelect = ({ category, setCategory }: CategorySelectProps) => {
     const { t } = useTranslation()
 
-    const options = categories.map(category => {
+    const options = categories.map(category => { // TODO: добавить перевод в сам select
         return {
             label: t('categories.' + category),
             value: category
