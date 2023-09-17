491fd937e12925deceb7fb76288eff3f1d804dec
PaulChernin <pavel@chernin.ru>
2023-09-17 17:58:46 +0300

    refactor: use zustand localstorage middleware

--- src/hooks/profile/useProfileStore.ts
+++ src/hooks/profile/useProfileStore.ts
@@ -1,29 +1,18 @@
 import { User } from "@/types/User"
 import { create } from "zustand"
-
-const getStorageProfile = (): User | null => {
-    const str = sessionStorage.getItem('profile')
-    return str && JSON.parse(str)
-}
-
-const saveStorageProfile = (profile: User | null) => {
-    if (!profile) {
-        sessionStorage.removeItem('profile')
-        return
-    }
-    const str = JSON.stringify(profile)
-    sessionStorage.setItem('profile', str)
-}
+import { persist } from "zustand/middleware"
 
 interface ProfileState {
     profile: User | null,
     setProfile: (profile: User | null) => void
 }
 
-export const useProfileStore = create<ProfileState>()((set) => ({
-    profile: getStorageProfile(),
-    setProfile: profile => {
-        saveStorageProfile(profile)
-        set(() => ({ profile: profile }))
-    }
-}))
+export const useProfileStore = create<ProfileState>()(
+    persist(
+        (set) => ({
+            profile: null,
+            setProfile: profile => set(() => ({ profile: profile }))
+        }),
+        { name: 'profile-storage' }
+    )
+)
