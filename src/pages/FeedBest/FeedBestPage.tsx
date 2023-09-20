a28f80042498c1ee211971b013ff0fa543fbdee7
PaulChernin <pavel@chernin.ru>
2023-09-20 20:24:25 +0300

    add loading

--- src/pages/FeedBest/FeedBestPage.tsx
+++ src/pages/FeedBest/FeedBestPage.tsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react"
 import { getBestReviews } from "./api/api"
 import PreviewsList from "../../components/PreviewsList/PreviewsList"
 import { Review } from "@/types/Review"
+import { Center, Spinner } from "@chakra-ui/react"
 
 const FeedBestPage = () => {
     const [previews, setPreviews] = useState<Array<Review>>([])
+    const [isLoading, setIsLoading] = useState(false)
 
     const load = async () => {
+        setIsLoading(true)
         setPreviews(await getBestReviews())
+        setIsLoading(false)
     }
     
     useEffect(() => {
@@ -15,6 +19,7 @@ const FeedBestPage = () => {
     }, [])
     
     return <>
+        {isLoading && <Center><Spinner/></Center>}
         <PreviewsList previews={previews} />
     </>
 }
--- src/pages/FeedTags/FeedTagsPage.tsx
+++ src/pages/FeedTags/FeedTagsPage.tsx
@@ -3,19 +3,25 @@ import { getReviewsByTag } from "./api/api"
 import PreviewsList from "../../components/PreviewsList/PreviewsList"
 import TagsCloud from "./components/TagsCloud"
 import { Review } from "@/types/Review"
-import { VStack } from "@chakra-ui/react"
+import { Center, Spinner, VStack } from "@chakra-ui/react"
 
 const FeedTagsPage = () => {
     const [previews, setPreviews] = useState<Array<Review>>([])
+    const [isLoading, setIsLoading] = useState(false)
 
     const load = async (tag: string) => {
+        setIsLoading(true)
         setPreviews(await getReviewsByTag(tag))
+        setIsLoading(false)
     }
     
     return <>
         <VStack gap={4}>
             <TagsCloud select={load} />
-            <PreviewsList previews={previews} />
+            {isLoading
+                ? <Center><Spinner/></Center>
+                : <PreviewsList previews={previews} />
+            }
         </VStack>
     </>
 }
